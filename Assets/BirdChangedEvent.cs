96e3761896e7cd9841f0e585d0a6204b8909fb0c
Eivind Vegsundvåg <eivindveg@hotmail.com>
2014-05-14 13:11:43 +0200

    Created bird changed event. not subscribable.

---
+++ Assets/BirdChangedEvent.cs
@@ -0,0 +1,47 @@
+﻿namespace Assets
+{
+    using System;
+
+    using UnityEngine;
+
+    public class BirdChangedEvent : EventArgs
+    {
+        #region Fields
+
+        private readonly GameObject bird;
+
+        private readonly BirdBehaviour.BirdState state;
+
+        #endregion
+
+        #region Constructors and Destructors
+
+        public BirdChangedEvent(GameObject bird, BirdBehaviour.BirdState state)
+        {
+            this.bird = bird;
+            this.state = state;
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        public GameObject Bird
+        {
+            get
+            {
+                return this.bird;
+            }
+        }
+
+        public BirdBehaviour.BirdState State
+        {
+            get
+            {
+                return this.state;
+            }
+        }
+
+        #endregion
+    }
+}
