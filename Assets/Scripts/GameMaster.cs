ce5f13a5ccfd1a62c8f60b37d5e9d127c288cb7b
Eivind Vegsundvåg <eivindveg@hotmail.com>
2014-05-15 16:32:58 +0200

    Add high score functionality

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -1,20 +1,56 @@
-﻿using UnityEngine;
-
-namespace Assets.Scripts
+﻿namespace Assets.Scripts
 {
+    using UnityEditor;
+
+    using UnityEngine;
+
     public class GameMaster : MonoBehaviour
     {
+        #region Constants
+
+        private const string DefaultHighScoreText = "Highscore: ";
+
+        private const string DefaultScoreText = "Score: ";
+
+        private const string Separator = "::";
+
+        #endregion
+
+        #region Fields
+
+        public GUIText ScoreGuItext;
+
+        private int highScore;
+
+        private bool isClear;
+
+        private string levelPrefsKey;
 
         private int remainingPigs;
-		public GUIText scoreGUItext;
-		private string defaultScoreText = "Score: ";
+
+        #endregion
+
+        #region Public Properties
+
         public int Score { get; private set; }
 
-        // Use this for initialization
-        void Start ()
+        #endregion
+
+        #region Public Methods and Operators
+
+        public void CheckHighscore()
         {
-            this.Score = 0;
-			scoreGUItext.text = defaultScoreText + Score;
+            if (this.Score > this.GetHighScore())
+            {
+                PlayerPrefs.SetInt("HighScore", this.Score);
+            }
+        }
+
+        public int GetHighScore()
+        {
+            string key = this.levelPrefsKey + Separator + "HighScore";
+
+            return PlayerPrefs.HasKey(key) ? PlayerPrefs.GetInt(key) : 0;
         }
 
         public void RegisterBrick(BrickCollision brick)
@@ -27,20 +63,11 @@ namespace Assets.Scripts
             }
         }
 
-        public void CheckHighscore()
-        {
-            if (this.Score > GetHighScore())
-            {
-                PlayerPrefs.SetInt("HighScore", this.Score);
-            }
-        }
+        #endregion
 
-        public int GetHighScore()
-        {
-            return PlayerPrefs.HasKey("HighScore") ? PlayerPrefs.GetInt("HighScore") : 0;
-        }
+        #region Methods
 
-        void BlockDeathEventHandler(BlockDeathEvent e)
+        private void BlockDeathEventHandler(BlockDeathEvent e)
         {
             this.Score += (int)e.Value;
             if (e.Type == BrickCollision.BrickType.Pig)
@@ -53,5 +80,23 @@ namespace Assets.Scripts
                 }
             }
         }
+
+        private void Start()
+        {
+            string currentScene = EditorApplication.currentScene;
+            currentScene = (currentScene.Substring((currentScene.Length - 1)));
+            this.levelPrefsKey = "level" + currentScene;
+
+            this.highScore = this.GetHighScore();
+            if (this.highScore > 0)
+            {
+                this.isClear = true;
+            }
+
+            this.Score = 0;
+            this.ScoreGuItext.text = DefaultScoreText + this.Score;
+        }
+
+        #endregion
     }
 }
