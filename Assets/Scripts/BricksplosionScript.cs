33a918b9c77e5cbde6f6ad028cc0a27f0f9bc486
Eivind Vegsundvåg <eivindveg@hotmail.com>
2014-05-15 13:32:45 +0200

    Add bricksplosion script.

---
+++ Assets/Scripts/BricksplosionScript.cs
@@ -0,0 +1,36 @@
+﻿namespace Assets.Scripts
+{
+    using UnityEngine;
+
+    public class BricksplosionScript : MonoBehaviour
+    {
+        #region Fields
+
+        public double LifeTime = 1.0;
+
+        private float currentLifetime;
+
+        #endregion
+
+        // Use this for initialization
+
+        #region Methods
+
+        private void Start()
+        {
+            this.currentLifetime = 0;
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            this.currentLifetime += Time.deltaTime;
+            if (this.currentLifetime >= this.LifeTime)
+            {
+                DestroyImmediate(this.gameObject);
+            }
+        }
+
+        #endregion
+    }
+}
