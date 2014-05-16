6f8aeb9203c07d2f3d902f2d02391b44f0840832
Mats Harwiss <rcdriftnitro2@gmail.com>
2014-05-16 13:47:27 +0200

    All sounds and soundscripts added. updated lvls and pigs,

---
+++ Assets/Scripts/SlingshotSound.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlingshotSound : MonoBehaviour {
+
+	public AudioClip slingSound;
+	public AudioClip flyingSound;
+	
+
+	public void playSling(){
+		gameObject.GetComponent<SoundSys>().PlaySoundOnce (slingSound);
+	}
+
+	public void playSvoosj(){
+		gameObject.GetComponent<SoundSys>().PlaySoundOnce (flyingSound);
+	}
+}
+	
---
+++ Assets/Scripts/Speak.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Speak : MonoBehaviour {
+	
+	public AudioClip voice_1; //Lydklippene det skal velges fra
+	public AudioClip voice_2;
+	public AudioClip voice_3;
+	public AudioClip voice_4;
+	
+	public float volume = 10;
+
+	public float minPause = 1; //Minimum tid i sekunder før neste lyd kan spilles
+	public float maxPause = 8; //Maksimum tid i sekunder før neste lyd spilles
+	
+
+	private double timeCounter=0.0;
+	private double randomTime = 0.0;
+
+	AudioClip[] voices; //Liste med lydklipp
+
+
+	void Start () {
+		voices = new AudioClip[] {voice_1,voice_2,voice_3,voice_4};
+	}
+	
+
+	void Update () {
+		useVoice ();
+		timeCounter += Time.deltaTime;
+	}
+
+	//Spiller av en tilfeldig lyd og venter et tilfeldig antall sekunder
+	void useVoice(){
+		if (timeCounter > randomTime) {
+			SoundSys soundSys = GetComponent<SoundSys>();
+			randomTime = Random.Range (minPause, maxPause);
+			timeCounter = 0.0;
+			soundSys.PlaySoundOnce (voices [Random.Range (0, voices.Length)]);
+		}
+
+	}
+
+
+}
