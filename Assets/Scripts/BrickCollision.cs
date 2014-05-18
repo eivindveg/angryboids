f62ad7e62c1245fe022e1dc23cc65d5b3c5fcc3e
Runnetty1 <rcdriftnitro2@gmail.com>
2014-05-18 17:32:49 +0200

    Added sounds to blocks

--- Assets/Scripts/BrickCollision.cs
+++ Assets/Scripts/BrickCollision.cs
@@ -1,98 +1,112 @@
 ﻿namespace Assets.Scripts
 {
-    using UnityEngine;
-
-    public delegate void BlockDeathEventHandler(BlockDeathEvent e);
-
-
-    public class BrickCollision : MonoBehaviour
-    {
-        // Use this for initialization
-
-        #region Fields
-
-        public double Maxhp;
-
-        public Sprite[] Sprites = new Sprite[5];
-
-        public BrickType Type;
-
-        private double currentHp;
-
-        private SpriteRenderer _renderer;
-
-        #endregion
-
-        #region Public Events
-
-        public event BlockDeathEventHandler Death;
-
-        #endregion
-
-        #region Enums
-
-        public enum BrickType
-        {
-            Pig,
-
-            Block
-        }
-
-        #endregion
-
-        #region Methods
-
-        private void OnCollisionEnter2D(Collision2D collision)
-        {
-            //Calculate damage made by the collision
-            double magnitudeMass = collision.relativeVelocity.magnitude * (collision.gameObject.rigidbody2D.mass + this.rigidbody2D.mass);
-            Debug.Log(magnitudeMass);
-
-            if (magnitudeMass >= 1.5)
-            {
-                this.currentHp = this.currentHp - magnitudeMass;
-            }
-
-            if (this.currentHp <= 0)
-            {
-                this._renderer.sprite = this.Sprites[4];
-                this.OnDeath();
-                Destroy(this.gameObject);
-            }
-            if (this.currentHp < (this.Maxhp * 0.8) && this.currentHp > (this.Maxhp * 0.6))
-            {
-                this._renderer.sprite = this.Sprites[1];
-            }
-            else if (this.currentHp < (this.Maxhp * 0.6) && this.currentHp > (this.Maxhp * 0.4))
-            {
-                this._renderer.sprite = this.Sprites[2];
-            }
-            if (this.currentHp < (this.Maxhp * 0.4) && this.currentHp > (this.Maxhp * 0.2))
-            {
-                this._renderer.sprite = this.Sprites[3];
-            }
-        }
-
-        private void OnDeath()
-        {
-            GameObject poff = (GameObject)Instantiate(Resources.Load("poff"));
-            poff.transform.position = this.transform.position;
-            if (this.Death != null)
-            {
-                this.Death(new BlockDeathEvent(this.Type, this.gameObject));
-            }
-        }
-
-        private void Start()
-        {
-            // Register with GameMaster
-            FindObjectOfType<GameMaster>().RegisterBrick(this);
-
-            this._renderer = this.gameObject.GetComponent<SpriteRenderer>();
-            this._renderer.sprite = this.Sprites[0];
-            this.currentHp = this.Maxhp;
-        }
-
-        #endregion
-    }
+	using UnityEngine;
+	
+	public delegate void BlockDeathEventHandler(BlockDeathEvent e);
+	
+	
+	public class BrickCollision : MonoBehaviour
+	{
+		// Use this for initialization
+		
+		#region Fields
+		
+		public double Maxhp;
+		
+		public Sprite[] Sprites = new Sprite[5];
+		
+		public BrickType Type;
+		
+		private double currentHp;
+		
+		private SpriteRenderer _renderer;
+		
+		public AudioClip[] CollisionSound;
+		
+		#endregion
+		
+		#region Public Events
+		
+		public event BlockDeathEventHandler Death;
+		
+		#endregion
+		
+		#region Enums
+		
+		public enum BrickType
+		{
+			Pig,
+			
+			Block
+		}
+		
+		#endregion
+		
+		#region Methods
+		
+		private void OnCollisionEnter2D(Collision2D collision)
+		{
+			
+			
+			
+			//Calculate damage made by the collision
+			double magnitudeMass = collision.relativeVelocity.magnitude * (collision.gameObject.rigidbody2D.mass + this.rigidbody2D.mass);
+			Debug.Log(magnitudeMass);
+			
+			if (magnitudeMass >= 1.5)
+			{
+				this.currentHp = this.currentHp - magnitudeMass;
+				
+				if (this.Type == BrickType.Block && Time.timeSinceLevelLoad > 2f)
+				{
+					int soundIndex = Random.Range(0, this.CollisionSound.Length);
+					this.audio.PlayOneShot(this.CollisionSound[soundIndex]);
+				}
+			}
+			
+			if (this.currentHp <= 0)
+			{
+				this._renderer.sprite = this.Sprites[4];
+				this.OnDeath();
+				Destroy(this.gameObject);
+			}
+			if (this.currentHp < (this.Maxhp * 0.8) && this.currentHp > (this.Maxhp * 0.6))
+			{
+				this._renderer.sprite = this.Sprites[1];
+			}
+			else if (this.currentHp < (this.Maxhp * 0.6) && this.currentHp > (this.Maxhp * 0.4))
+			{
+				this._renderer.sprite = this.Sprites[2];
+			}
+			if (this.currentHp < (this.Maxhp * 0.4) && this.currentHp > (this.Maxhp * 0.2))
+			{
+				this._renderer.sprite = this.Sprites[3];
+			}
+		}
+		
+		private void OnDeath()
+		{
+			GameObject poff = (GameObject)Instantiate(Resources.Load("poff"));
+			poff.transform.position = this.transform.position;
+			if (this.Death != null)
+			{
+				this.Death(new BlockDeathEvent(this.Type, this.gameObject));
+			}
+		}
+		
+		private void Start()
+		{
+			// Register with GameMaster
+			FindObjectOfType<GameMaster>().RegisterBrick(this);
+			if(this.Type == BrickType.Block){
+				this.gameObject.AddComponent<AudioSource>();
+				this.audio.volume = 0.3f;
+			}
+			this._renderer = this.gameObject.GetComponent<SpriteRenderer>();
+			this._renderer.sprite = this.Sprites[0];
+			this.currentHp = this.Maxhp;
+		}
+		
+		#endregion
+	}
 }
